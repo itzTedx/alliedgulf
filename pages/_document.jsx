c4455e3ec9d4f5cb32a7550df11dbf679efd0331
melwin <zomboi.offi@gmail.com>
2022-10-24 17:28:51 +0530

    init

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -46,18 +46,14 @@ export default function Document() {
         </Script>
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: (
-              <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-TD9WJ26"
-                height="0"
-                width="0"
-                style="display:none;visibility:hidden"
-              ></iframe>
-            ),
-          }}
-        ></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TD9WJ26"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
