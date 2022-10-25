eb763f3761f922fb4c0749e92bc78218cc1437e3
melwin <zomboi.offi@gmail.com>
2022-10-25 11:38:35 +0530

    init

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
+import NextNProgress from "nextjs-progressbar";
+
 import Layout from "../components/Layout";
 
 import "../styles/globals.css";
@@ -9,6 +11,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
+        <NextNProgress
+          color="linear-gradient(90deg, #00b4d8 0%, #219ebc 100%);"
+          startPosition={0.2}
+          stopDelayMs={100}
+          height={5}
+          showOnShallow={true}
+          options={{ easing: "ease-in-out", speed: 200 }}
+        />
         <AnimatePresence mode="wait">
           <Component {...pageProps} />
         </AnimatePresence>
